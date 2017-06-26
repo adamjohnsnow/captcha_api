f471468c16d692709dd9b483c5490932536de25b
JayWebDevCom <jaiye.s@gmail.com>
2017-06-26 16:16:57 +0100

    transferred to imgAssoc

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var cors = require('cors')
 // var session = require('express-session');
 // var getAnswer = require('./assets/antworten')
-var Minigames = require('./assets/areaClick')
+var ImgAssoc = require('./assets/imgAssoc')
 
 app.set('port', (process.env.PORT || 7070))
 
@@ -17,24 +17,16 @@ app.listen(app.get('port'), function(){
   console.log('port is ' + app.get('port'))
 })
 
-// app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // next();
-// });
-
 app.get('/', function(req, res){
-  var capcha = new Minigames();
+  var capcha = new ImgAssoc();
   res.send(capcha.gameData);
 })
 
 app.post('/answer', function(req, res){
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   captcha = new Minigames()
   res.send(captcha.getSolution(req.query))
 })
 
 app.get('/test', function(req, res){
-  res.render('heroku_api_test', {})
+  res.render('api_test', {})
 })
