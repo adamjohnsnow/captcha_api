0a6cd645826214b09434b1fb651a780abc87c5d2
JayWebDevCom <jaiye.s@gmail.com>
2017-06-26 17:06:22 +0100

    do something with response

--- captchupFiles/captchup.js
+++ captchupFiles/captchup.js
@@ -12,7 +12,8 @@ $.get("https://arcane-depths-35133.herokuapp.com", function( data ) {
 function selectImage(event){
   var answer = 'id=' + gameKey + '&img=' + event.target.currentSrc
   $.post("https://arcane-depths-35133.herokuapp.com/answer?" + answer, function(response){
-    alert(response)
+    var apiResponse = response
+    return apiResponse
   })
 }
 
