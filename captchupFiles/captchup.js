312533df99590170195c62d03a74a1732cc2faa7
JayWebDevCom <jaiye.s@gmail.com>
2017-06-26 17:51:59 +0100

    appending to form hidden

--- captchupFiles/captchup.js
+++ captchupFiles/captchup.js
@@ -12,8 +12,7 @@ $.get("https://arcane-depths-35133.herokuapp.com", function( data ) {
 function selectImage(event){
   var answer = 'id=' + gameKey + '&img=' + event.target.currentSrc
   $.post("https://arcane-depths-35133.herokuapp.com/answer?" + answer, function(response){
-    $('#response').value(response)
-
+    $('#response').val(response)
   })
 }
 
