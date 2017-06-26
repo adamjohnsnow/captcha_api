0599c6afce7e939a76070235dc44e1c380682f7c
JayWebDevCom <jaiye.s@gmail.com>
2017-06-26 12:15:16 +0100

    functioning minigame returns imgur urls

---
+++ assets/areaClick.js
@@ -0,0 +1,36 @@
+var mains = [
+  { id: 201, img:'http://i.imgur.com/4AehZVQ.jpg', prompt: 'Click the Coke Label' },
+  { id: 204, img: 'http://i.imgur.com/QyU5v2N.jpg', prompt: 'Click on the number eight' },
+  { id: 202, img:'http://i.imgur.com/Nz68cPK.jpg', prompt: 'Click the Orange Letter' },
+  { id: 203, img: 'http://i.imgur.com/HoBl8Mq.jpg', prompt: 'Click on Michael Jordan\'s Armband'}
+]
+var solutions = [
+  { id: 201, xLowLimit: 138, xHighLimit: 181, yLowLimit: 57, yHighLimit: 125 },
+  { id: 202, xLowLimit: 163, xHighLimit: 226, yLowLimit: 58, yHighLimit: 124 },
+  { id: 203, xLowLimit: 304, xHighLimit: 341, yLowLimit: 207, yHighLimit: 245 },
+  { id: 204, xLowLimit: 287, xHighLimit: 348, yLowLimit: 38, yHighLimit: 104 }
+]
+
+var ClickArea = function(stubNumber){
+  var randomIndex = stubNumber || Math.floor(Math.random() * mains.length)
+  this.type = 'clickArea'
+  this.gameKey = mains[randomIndex].id;
+  this.gameData = mains[randomIndex];
+}
+
+ClickArea.prototype.getSolution = function(answer){
+  function findSolution(hash) {
+    return hash.id === answer[0];
+  }
+  var solution = solutions.find(findSolution);
+  return (
+    Number(answer[1].x) > solution.xLowLimit &&
+    Number(answer[1].x) < solution.xHighLimit &&
+    Number(answer[1].y) > solution.yLowLimit &&
+    Number(answer[1].y) < solution.yHighLimit
+  );
+}
+
+module.exports = ClickArea;
+
+// brandedCaptcha@#$123
--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var bodyparser = require('body-parser')
 var app = express();
 // var session = require('express-session');
 // var getAnswer = require('./assets/antworten')
-// var Minigames = require('./assets/minigames')
+var Minigames = require('./assets/areaClick')
 
 app.listen(7070, function(){
   console.log('port is 7070!')
 })
 
 app.get('/', function(req, res){
-  res.send('working')
+  var capcha = new Minigames();
+  res.send(capcha.gameData);
+})
+
+
+app.post('/answer', function(req, res){
+
+
 })
