1eef3bfb76fb37be515b431df226245238f4d7ee
JayWebDevCom <jaiye.s@gmail.com>
2017-06-26 12:58:35 +0100

    add view files and cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var bodyparser = require('body-parser')
 var app = express();
+var cors = require('cors')
 // var session = require('express-session');
 // var getAnswer = require('./assets/antworten')
 var Minigames = require('./assets/areaClick')
 
 app.set('port', (process.env.PORT || 7070))
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+app.use(cors());
 
 app.listen(app.get('port'), function(){
   console.log('port is ' + app.get('port'))
@@ -19,5 +24,10 @@ app.get('/', function(req, res){
 
 app.post('/answer', function(req, res){
 
+})
 
+app.get('/test', function(req, res){
+  res.render('index', {
+    word: 'Hello World'
+  })
 })
