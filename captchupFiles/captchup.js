4031a52c5feab0f637c2ace5c91fb0ab691aee88
JayWebDevCom <jaiye.s@gmail.com>
2017-06-27 15:43:05 +0100

    positioning modal

--- captchupFiles/captchup.js
+++ captchupFiles/captchup.js
@@ -28,7 +28,7 @@ var captchup = function() {
       closeOnEscape: false,
       resizable: false,
       Height: 700,
-      position: { my: "center center", at: "center top", of: window }
+      position: { my: "center", at: "center", of: window }
     })
   })
 };
