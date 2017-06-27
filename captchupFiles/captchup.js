5815dfe48b08b93285eba7160fe301c196caa862
JayWebDevCom <jaiye.s@gmail.com>
2017-06-27 12:00:50 +0100

    closes on true, reloads on false

--- captchupFiles/captchup.js
+++ captchupFiles/captchup.js
@@ -12,7 +12,7 @@ $.get("https://arcane-depths-35133.herokuapp.com", function( data ) {
 function selectImage(event){
   var answer = 'id=' + gameKey + '&img=' + event.target.currentSrc
   $.post("https://arcane-depths-35133.herokuapp.com/answer?" + answer, function(response){
-    $('#response').val(response)
+    response == true ? $( "#captchup" ).dialog( "close" ); : captchup();
   })
 }
 
